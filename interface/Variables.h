2d999da8990cae95402eaec4f0ec310b8094fa7e
Nikolaos Vassakis <nvassaki@t3ui06.psi.ch>
2025-09-29 19:24:48 +0200

    Adding lumi info for dedicated runs

--- interface/Variables.h
+++ interface/Variables.h
@@ -959,7 +959,7 @@ public:
     //input = fopen ("input/run_ls_instlumi_pileup_2017.txt","r");
     //input = fopen ("input/run_ls_instlumi_pileup_phase1.txt","r");
     // input = fopen ("input/run_ls_intlumi_pileup_phase1_Run3.txt","r");
-    input = fopen ("input/run_ls_intlumi_pileup_phase1_Run3_2025.txt","r");
+    input = fopen ("input/2025_runs.txt","r");
     a = 1;
     while (a==1) {
       a = fscanf (input, "%d", &run);
